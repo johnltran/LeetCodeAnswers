eb00d409896c5a6b32885c581ffbeabd2eaf55c4
John Tran <johntran627@gmail.com>
2021-09-08 17:33:29 -0500

    p1,2

---
+++ Problems_1_to_50/Problem_2.java
@@ -0,0 +1,29 @@
+// Problem 2: Add Two Numbers 
+
+public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+  if (l1 == null) return l2;
+  if (l2 == null) return l1;
+
+  ListNode dummy = new ListNode(0);
+  ListNode cur = dummy;
+  int carry = 0;
+  while (l1 != null || l2 != null || carry != 0) {
+    int sum = carry;
+
+    if (l1 != null) {
+      sum += l1.val;
+      l1 = l1.next;
+    }
+
+    if (l2 != null) {
+      sum += l2.val;
+      l2 = l2.next;
+    }
+
+    cur.next = new ListNode(sum % 10);
+    cur = cur.next;
+    carry = sum / 10;
+  }
+
+  return dummy.next;
+}
