812add224a1ed893d78e9ce55cdabd6e150b7c9e
John Tran <johntran627@gmail.com>
2021-09-10 23:46:01 -0500

    p5&6

--- Problems_1_to_50/Problem_3.java
+++ Problems_1_to_50/Problem_3.java
@@ -9,14 +9,11 @@ public int lengthOfLongestSubstring(String s) {
   int len = s.length();
   if (len == 0) return 0;
 
+  int result = 1; 
   Map<Character, Integer> cache = new HashMap<>();
-  int result = 1;
   int start = 0;
   for (int i = 0; i < len; ++i) {
     char ch = s.charAt(i);
-
-    // If the current char is in the map,
-    // then the substring will start over at the last index of that char + 1
     if (cache.containsKey(ch))
       start = Math.max(start, cache.get(ch) + 1);
 
---
+++ Problems_1_to_50/Problem_6.java
@@ -0,0 +1,10 @@
+/*
+  Problem 6: ZigZag Conversion
+
+  Time:
+  Space:
+*/
+
+public String convert(String s, int numRows) {
+  
+}
