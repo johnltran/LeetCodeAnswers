fa9655c520052b5a54b71850e4fd86cd9215961a
John Tran <johntran627@gmail.com>
2021-09-13 17:26:10 -0500

    11 & 12

--- Problems_1_to_50/Problem_9.java
+++ Problems_1_to_50/Problem_9.java
@@ -1,6 +1,20 @@
 /*
-  Problem 9:
+  Problem 9: Palindrome Number
 
-  Time:
-  Space:
+  Time: O(log N)
+  Space: O(1)
 */
+
+public boolean isPalindome(int x) {
+  if (x == 0) return true;
+  if (x < 0 || x % 10 == 0) return false;
+
+  int half = 0;
+  while (x > half) {
+    half = half * 10 + (x % 10);
+    x /= 10;
+  }
+
+  // after the loop, half >= x
+  return x == half || x == half/10; 
+}
